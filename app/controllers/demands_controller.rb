6db763753ccef90108c50975a5b8cff932034a09
Eduardo Moreira <moreira.edu@hotmail.com>
2015-06-26 09:19:56 -0300

    Creating method to validate topics

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -23,6 +23,24 @@ class DemandsController < ApplicationController
     @topic = Topic.new
   end
 
+  def validate_topic
+    @demand = Demand.find(params[:demand_id])
+    @topics = @demand.topics.where(:sent => true)
+  end
+
+  def approve_topic
+    @topic = Topic.find(params[:topic_id])
+    @topic.accepted = params[:accepted]
+    @topic.save
+    redirect_to '/validate_topic?demand_id='+@topic.demand.id.to_s
+  end
+
+  def validated_topics
+    @demand = Demand.find(params[:demand_id])
+    @topics = @demand.topics.where(:accepted => true)
+
+  end
+
   def new_topic
     
   end
